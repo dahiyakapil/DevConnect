ec9e6a8818f2c4fcb79adb07e1a116bd2334e3a3
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-02-10 13:15:31 +0530

    Order of routes matters

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use("/hello/123", (req, res) => {
+    res.send("Hello from /hello/123")
+})
+
+app.use("/hello", (req, res) => {
+    res.send("Hello from /hello")
+})
+
+app.use("/xyz", (req, res) => {
+    res.send("XYZ ROUTE")
+})
 
 app.use("/", (req, res) => {
     res.send("Hello from DEV CONNECT");
