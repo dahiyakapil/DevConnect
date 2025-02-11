476410eb8b3723c2970547a11dae52f0662875f5
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-02-11 11:33:10 +0530

    Route Methods GET, POST, DELETE, PUT and route order matters in routing

--- src/index.js
+++ src/index.js
@@ -3,21 +3,29 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use("/hello/123", (req, res) => {
-    res.send("Hello from /hello/123")
+
+
+app.use("/user", (req, res) => {
+    res.send("Route Order Matters !!!")
 })
 
-app.use("/hello", (req, res) => {
-    res.send("Hello from /hello")
+app.get("/user", (req, res) => {
+    res.send({ "firstName": "Kapil", "lastName": "Dahiya" });
 })
 
-app.use("/xyz", (req, res) => {
-    res.send("XYZ ROUTE")
+app.post("/user", (req, res) => {
+    res.send("User data saved to the databse successfully!")
 })
 
-app.use("/", (req, res) => {
-    res.send("Hello from DEV CONNECT");
+app.delete("/user", (req, res) => {
+    res.send("User data deleted successfully!")
 })
+
+app.put("/user", (req, res) => {
+    res.send("User data updated Successfully !!!")
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
