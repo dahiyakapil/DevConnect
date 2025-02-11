e1d2a858101299cd5224aa29b28a72c85d2bf5b6
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-02-11 13:29:10 +0530

    Route Paramters.. req.parmas Used to define dynamic parts of the URL.

--- src/index.js
+++ src/index.js
@@ -4,41 +4,31 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 
+//^ Route Parameters
 
-//^ Route paths based on string patterns Advance Routing using wildcard characters dynamic and flexible routing
-
-
-// in ? : abc, ac --> b is optional here
-app.get("/ab?c", (req, res) => {
-    res.send({ "firstName": "Kapil", "lastName": "Dahiya" });
+// http://localhost:4000/user/abc
+app.get("/user/:userId", (req, res) => {
+    console.log(req.params) // { userId: 'abc' }
+    console.log(req.params.userId) // abc
+    res.send({ "firstName": "Kapil", "lastName": "Dahiya" })
 })
 
-// abc, abbbbc, b is must ... abcc(not work)
-app.get("/ab+c", (req, res) => {
-    res.send({ "firstName": "Kapil", "lastName": "Dahiya" });
+//* :id is a route parameter, which acts as a placeholder.
+
+// Multiple Route Parameters
+
+// http://localhost:4000/user/abcd/books/5
+app.get("/user/:userId/books/:bookId", (req, res) => {
+    console.log(req.params); // { userId: 'abc', bookId: '5' }
+    console.log(req.params.userId) // abcd
+    console.log(req.params.bookId) // 5
+    res.send({ "firstName": "Kapil", "lastName": "Dahiya" })
 })
 
-// abcd, abKAPILcd 
-app.get("/ab*cd", (req, res) => {
-    res.send({ "firstName": "Kapil", "lastName": "Dahiya" });
+app.get("/flight/:from-:to", (req, res) => {
+    console.log(req.params); // { from: 'LAX', to: 'SFO' }
+    res.send("Flight Data fetched")
 })
-
-
-//^ Route paths based on regular expressions
-
-// It will check if a is there in the url or not if not present then it will throw the error
-// http://localhost:4000/mango
-app.get(/a/, (req, res) => {
-    res.send({ "firstName": "Kapil", "lastName": "Dahiya" });
-})
-
-
-// in this url anything can come before fly.. but fly should be there then it will work.... and writing anything after fly1 it will throw error
-// http://localhost:4000/bKAPILcdafly
-app.get(/.*fly$/, (req, res) => {
-    res.send({ "firstName": "Kapil", "lastName": "Dahiya" });
-})
-
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
