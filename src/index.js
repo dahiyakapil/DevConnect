14de2429614a56c40a164b3bec32d41da187e5ab
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-02-11 13:47:02 +0530

    Query paramters are key value pairs that are appended to the URL after a ? symbol.

--- src/index.js
+++ src/index.js
@@ -4,31 +4,27 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 
-//^ Route Parameters
+//^ Query Parameters
 
-// http://localhost:4000/user/abc
-app.get("/user/:userId", (req, res) => {
-    console.log(req.params) // { userId: 'abc' }
-    console.log(req.params.userId) // abc
-    res.send({ "firstName": "Kapil", "lastName": "Dahiya" })
+app.get("/user", (req, res) => {
+    console.log(req.query) // { id: '10', age: '24' } http://localhost:4000/user?id=10&age=24
+    res.send({ "firstName": "kapil", "lastName": "Dahiya" })
 })
 
-//* :id is a route parameter, which acts as a placeholder.
+// Query Parameters with Arrays
+app.get("/filter", (req, res) => {
+    console.log(req.query) // { colors: [ 'red', 'blue' ] }
+    res.send(`Colors: ${req.query.colors}`);
+});
 
-// Multiple Route Parameters
 
-// http://localhost:4000/user/abcd/books/5
-app.get("/user/:userId/books/:bookId", (req, res) => {
-    console.log(req.params); // { userId: 'abc', bookId: '5' }
-    console.log(req.params.userId) // abcd
-    console.log(req.params.bookId) // 5
-    res.send({ "firstName": "Kapil", "lastName": "Dahiya" })
-})
-
-app.get("/flight/:from-:to", (req, res) => {
-    console.log(req.params); // { from: 'LAX', to: 'SFO' }
-    res.send("Flight Data fetched")
-})
+// Query Parameters in Pagination
+app.get("/users", (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    console.log(req.query) // { page: '2', limit: '9' }
+    res.send(`Page: ${page}, Limit: ${limit}`);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
