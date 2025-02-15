a848b583826461864aacaf15fd9a4ce458da5e55
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-02-15 13:53:49 +0530

    Route Handlers and next() function

--- src/index.js
+++ src/index.js
@@ -4,27 +4,20 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 
-//^ Query Parameters
+//^ Middlewares and Error Handlers
 
-app.get("/user", (req, res) => {
-    console.log(req.query) // { id: '10', age: '24' } http://localhost:4000/user?id=10&age=24
-    res.send({ "firstName": "kapil", "lastName": "Dahiya" })
+//& Route Handlers
+
+app.use("/user", (req, res, next) => {
+    console.log("Route Handler -> 1")
+    next();
+    // res.send("Response 1")
+
+}, (req, res, next) => {
+    console.log("Route Handler -> 2")
+    res.send("Response 2")
+    next();
 })
-
-// Query Parameters with Arrays
-app.get("/filter", (req, res) => {
-    console.log(req.query) // { colors: [ 'red', 'blue' ] }
-    res.send(`Colors: ${req.query.colors}`);
-});
-
-
-// Query Parameters in Pagination
-app.get("/users", (req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-    console.log(req.query) // { page: '2', limit: '9' }
-    res.send(`Page: ${page}, Limit: ${limit}`);
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
