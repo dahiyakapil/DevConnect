f414c612efb8d4c8c1f19a035643b9b71360cf44
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-03-07 09:50:18 +0530

    Created API without validaitons Find user by email FInd user by id (req.body or req.params) Delete user by id (req.body or req.params) updated user by id (req.body or req.params)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ dotenv.config()
 import express from "express";
 import { connectDB } from "./config/connectDB.js";
 import User from "./models/user.models.js";
+import { ReturnDocument } from "mongodb";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -23,11 +24,107 @@ app.post("/signup", async (req, res) => {
 
     try {
         await user.save();
-        res.send("User created succefully")
+        res.status(201).json({ message: "User created successfully", user });
+
     } catch (error) {
         res.status(404).send("Error in creating the user")
     }
 })
+
+// find the user by email
+app.get("/user", async (req, res) => {
+    const user = req.body.email;
+    console.log(user)
+
+    try {
+        const userFound = await User.find({ email: user })
+        if (userFound.length === 0) {
+            res.status(404).send("User not found");
+        }
+        res.send(userFound)
+    } catch (error) {
+        res.send("Something went wrong")
+    }
+})
+
+// find all user /feed
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (error) {
+        res.status(404).send("Something went wrong")
+    }
+})
+
+// Find user by _id
+app.get("/user/:id", async (req, res) => {
+    const userId = req.params.id; // Get _id from request params
+    console.log(req.params)
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send("Something went wrong");
+    }
+});
+
+
+// Delete API
+app.delete("/user", async (req, res) => {
+    const userId = req.body;
+    try {
+        // const user = await User.findByIdAndDelete(userId);
+        const user = await User.findByIdAndDelete({ _id: userId });
+        res.send("User deleted Successfully");
+    } catch (error) {
+        res.status(500).send("Something went wrong");
+    }
+})
+
+app.delete("/user/:id", async (req, res) => {
+    const userId = req.params.id;
+    try {
+        // const user = await User.findByIdAndDelete(userId);
+        const user = await User.findByIdAndDelete({ _id: userId });
+        res.send("User deleted Successfully");
+    } catch (error) {
+        res.status(500).send("Something went wrong");
+    }
+})
+
+
+// Update API
+
+app.patch("/user", async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        const user = await User.findByIdAndUpdate({ _id: userId }, data, { returnDocument: "after" });
+        console.log(user)
+        res.send("User Updated Successfully")
+    } catch (error) {
+        res.status(500).send("Something went wrong");
+    }
+})
+app.patch("/user/:id", async (req, res) => {
+    const userId = req.params.id;
+    const data = req.body;
+    try {
+        const user = await User.findByIdAndUpdate(userId , data, { returnDocument: "after" });
+        console.log(user)
+        res.send("User Updated Successfully")
+    } catch (error) {
+        res.status(500).send("Something went wrong");
+    }
+})
+
+
+
+
 
 connectDB()
     .then(() => {
