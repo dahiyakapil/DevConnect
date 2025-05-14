6e5df21f3c5af8d69f518a38788418c707a67ff6
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-05-14 18:10:00 +0530

    changes made..

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose"
 export const connectDB = async () => {
 
     const str = "mongodb+srv://kapil:HMyG4pUAob5RSDew@devconnect.2ahsn.mongodb.net/?retryWrites=true&w=majority&appName=devConnect";
-    console.log(str)
     try {
         const connectionInstance = await mongoose.connect(str);
         console.log(`MongoDB connected Succesfully || ${connectionInstance.connection.host}`)
